9f23208a59e61baf4f7b6c68a6bc77c0628da676
mcatalano26 <mattcat26@gmail.com>
2021-04-20 15:36:09 -0500

    everything is working

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def showPosts():
     swearwords_df = pd.read_csv('files/edited-swear-words.csv')
     swearwords = swearwords_df.swear.tolist()
     features = ['profanity', 'length', 'adjWordScore', 'NER_count', 'NER_match', 'WordScore', 'WholeScore', 'contains_url', 'no_url_WordScore', 'no_url_WholeScore', 'WordScoreNoStop', 'WholeScoreNoStop', 'no_url_or_stops_WholeScore', 'no_url_or_stops_WordScore']
-    our_model = load("latest_model.pkl", compression="lzma", set_default_extension=False)
+    our_model = load("updated_model.pkl", compression="lzma", set_default_extension=False)
     punctuation_lst = [',', '.', '!', '?', '<', '>', '/', ':', ';', '\'', '\"', '[', '{', ']', '}', '|', '\\', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+']
 
     
@@ -128,7 +128,7 @@ def scoreComment():
     swearwords_df = pd.read_csv('files/edited-swear-words.csv')
     swearwords = swearwords_df.swear.tolist()
     features = ['profanity', 'length', 'adjWordScore', 'NER_count', 'NER_match', 'WordScore', 'WholeScore', 'contains_url', 'no_url_WordScore', 'no_url_WholeScore', 'WordScoreNoStop', 'WholeScoreNoStop', 'no_url_or_stops_WholeScore', 'no_url_or_stops_WordScore']
-    our_model = load("latest_model.pkl", compression="lzma", set_default_extension=False)
+    our_model = load("updated_model.pkl", compression="lzma", set_default_extension=False)
     punctuation_lst = [',', '.', '!', '?', '<', '>', '/', ':', ';', '\'', '\"', '[', '{', ']', '}', '|', '\\', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+']
 
     #Lime stuff to add...probably should make this better so it isn't run every time
